74c673e3cd4177de198a23d42c1de257fc7af445
2944461376 <2944461376@users.noreply.github.com>
2025-12-15 22:37:28 +0800

    Support PORT env variable

--- run_backend.py
+++ run_backend.py
@@ -5,9 +5,10 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "backend.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=True
     )
